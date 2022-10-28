797a4d468247664cafed3cb887bc1cdf6bf5ac2b
maurice_oluoch <oluochstudy@gmail.com>
2022-10-28 10:43:38 +0300

    setting up general styles

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { VerifyComponent } from './components/verify/verify.component';
+import {ResetComponent} from "./components/reset/reset.component";
+import {ConfirmResetComponent} from "./components/confirm-reset/confirm-reset.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: 'password_reset',
+        component: ResetComponent
+      },
+      {
+        path: 'confirm_reset',
+        component: ConfirmResetComponent
+      },
+      {
+        path: 'verify',
+        component: VerifyComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}

227cc1152211dda3bb43b209b00f961c453f6e1d
maurice_oluoch <oluochstudy@gmail.com>
2023-01-05 09:19:08 +0300

    Accounts UI ready

--- src/app/dashboard/components/account/account.component.ts
+++ src/app/dashboard/components/account/account.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent implements OnInit {
+  constructor(private formBuilder: UntypedFormBuilder) {}
 
-  constructor() { }
+  userInfoForm = this.formBuilder.group({
+    first_name: ['', Validators.required],
+    last_name: ['', Validators.required],
+    phone_number: ['', Validators.required],
+    email: ['', Validators.required],
+    date_of_birth: [''],
+    id_number: [''],
+    gender: [''],
+  });
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

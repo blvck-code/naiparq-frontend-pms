6a6ee076dcbc8fdf5127ddfceb29e1b076ccc3ff
maurice_oluoch <oluochstudy@gmail.com>
2023-01-09 14:18:54 +0300

    added analytics UI

--- src/app/dashboard/components/analytics/analytics.component.ts
+++ src/app/dashboard/components/analytics/analytics.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { CountryRenewableElectricityItem } from '../../services/ChartData';
+import { CountryRenewableElectricity } from '../revenues/CountryRenewableElectricity';
+import {
+  IgxCategoryChartComponent,
+  IgxLegendComponent,
+} from 'igniteui-angular-charts';
 
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
-  styleUrls: ['./analytics.component.scss']
+  styleUrls: ['./analytics.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AnalyticsComponent implements OnInit {
+  @ViewChild('legend', { static: true })
+  private legend!: IgxLegendComponent;
+  @ViewChild('chart', { static: true })
+  private chart!: IgxCategoryChartComponent;
 
-  constructor() { }
+  constructor(private _detector: ChangeDetectorRef) {}
 
-  ngOnInit(): void {
+  //@ts-ignore
+  private _countryRenewableElectricity: CountryRenewableElectricity = null;
+  public get countryRenewableElectricity(): CountryRenewableElectricity {
+    if (this._countryRenewableElectricity == null) {
+      this._countryRenewableElectricity = new CountryRenewableElectricity();
+    }
+    return this._countryRenewableElectricity;
   }
 
+  ngOnInit(): void {}
 }

51ec7e5e4ac82ff4ea8fd53e86654b17aa572fec
maurice_oluoch <oluochstudy@gmail.com>
2023-01-09 11:53:08 +0300

    revenue graph ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,36 @@
-import {NgModule, isDevMode} from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
 import { OnlineStatusModule } from 'ngx-online-status';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { localStorageSync } from 'ngrx-store-localstorage';
+import { CookieService } from 'ngx-cookie-service';
 
 // NgRx
-import {ActionReducer, MetaReducer, StoreModule} from "@ngrx/store";
-import {StoreDevtoolsModule} from "@ngrx/store-devtools";
-import {localStorageSync} from "ngrx-store-localstorage";
-import {SharedModule} from "./shared/shared.module";
-import {AuthModule} from "./auth/auth.module";
-import {EffectsModule} from "@ngrx/effects";
-import {AuthInterceptor} from "./interceptors/auth.interceptor";
-import {CookieService} from "ngx-cookie-service";
-import {ErrorInterceptor} from "./interceptors/error.interceptor";
-import {environment} from "../environments/environment";
+import { ActionReducer, MetaReducer, StoreModule } from '@ngrx/store';
+
+import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { SharedModule } from './shared/shared.module';
+
+import { EffectsModule } from '@ngrx/effects';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
+import { environment } from '../environments/environment';
 
 // Setting user details on localstorage
-export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
-  return localStorageSync({keys: ['userCenter'], rehydrate: true})(reducer);
+export function localStorageSyncReducer(
+  reducer: ActionReducer<any>
+): ActionReducer<any> {
+  return localStorageSync({ keys: ['userCenter'], rehydrate: true })(reducer);
 }
 const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -35,12 +38,12 @@ const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
     AuthModule,
     SharedModule,
     OnlineStatusModule,
-    StoreModule.forRoot({}, {metaReducers}),
+    StoreModule.forRoot({}, { metaReducers }),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({
       name: 'Naiparq',
       maxAge: 25,
-      logOnly: environment.production
+      logOnly: environment.production,
     }),
   ],
   providers: [
@@ -56,6 +59,6 @@ const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
       multi: true,
     },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/dashboard/components/revenues/CountryRenewableElectricity.ts
@@ -0,0 +1,98 @@
+export class CountryRenewableElectricityItem {
+  public constructor(init: Partial<CountryRenewableElectricityItem>) {
+    Object.assign(this, init);
+  }
+
+  public year: string | undefined;
+  public income: number | undefined;
+  public expense: number | undefined;
+}
+export class CountryRenewableElectricity extends Array<CountryRenewableElectricityItem> {
+  public constructor() {
+    super();
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 14`,
+        income: 21,
+        expense: 100,
+      })
+    );
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 15`,
+        income: 26,
+        expense: 24,
+      })
+    );
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 16`,
+        income: 29,
+        expense: 28,
+      })
+    );
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 17`,
+        income: 32,
+        expense: 26,
+      })
+    );
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 18`,
+        income: 47,
+        expense: 38,
+      })
+    );
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 19`,
+        income: 46,
+        expense: 31,
+      })
+    );
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 20`,
+        income: 50,
+        expense: 19,
+      })
+    );
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 21`,
+        income: 90,
+        expense: 52,
+      })
+    );
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 22`,
+        income: 132,
+        expense: 50,
+      })
+    );
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 23`,
+        income: 134,
+        expense: 34,
+      })
+    );
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 24`,
+        income: 134,
+        expense: 34,
+      })
+    );
+    this.push(
+      new CountryRenewableElectricityItem({
+        year: `Apr 24`,
+        income: 96,
+        expense: 38,
+      })
+    );
+  }
+}

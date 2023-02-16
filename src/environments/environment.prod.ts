386b26bbf9944874952d323f7027850f5f4d4756
maurice <maurice.oluoch@naiparq.co.ke>
2023-02-16 10:32:59 +0300

    working on prod error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -33,6 +33,10 @@ export const environment = {
 
   // Device Module URLS
   naiparqDevicesList: naiparqBaseURL + 'asset/device',
+
+  // Naiparq Blog
+  naiparqCreateBlog: naiparqBaseURL + 'blog/article/',
+  naiparqBlogList: naiparqBaseURL + 'blog/article',
 };
 
 /*

5c1d5f1453f30f8353f87b4cef423b48edab8226
maurice <maurice.oluoch@naiparq.co.ke>
2023-02-22 13:05:19 +0300

    added logics for adding new space pricing

--- src/app/dashboard/components/premises/premises.component.ts
+++ src/app/dashboard/components/premises/premises.component.ts
@@ -4,6 +4,7 @@ import { StoreService } from '../../state/store.service';
 import { SpaceModel } from '../../models/spaces.model';
 import * as spaceActions from '../../state/actions/spaces.actions';
 import { Store } from '@ngrx/store';
+import { UntypedFormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-premises',
@@ -11,7 +12,23 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./premises.component.scss'],
 })
 export class PremisesComponent implements OnInit {
-  constructor(private storeSrv: StoreService, private store: Store) {}
+  pricingForm = this.fb.group({
+    min_time: ['', Validators.required],
+    max_time: ['', Validators.required],
+    price: ['', Validators.required],
+  });
+
+  steps = {
+    pricing: true,
+    previewPrice: false,
+    spaceDetails: false,
+  };
+
+  constructor(
+    private storeSrv: StoreService,
+    private store: Store,
+    private fb: UntypedFormBuilder
+  ) {}
 
   spaces$: Observable<SpaceModel[]> = this.storeSrv.getSpaces();
   spacesLoading$: Observable<boolean> = this.storeSrv.spacesLoading();
@@ -27,5 +44,30 @@ export class PremisesComponent implements OnInit {
 
   getPremises(): void {
     this.store.dispatch(new spaceActions.LoadSpaces());
+  }
+
+  goBack(): void {
+    this.steps = {
+      pricing: true,
+      previewPrice: false,
+      spaceDetails: false,
+    };
+  }
+
+  confirmPriceSchedule(): void {
+    this.steps = {
+      pricing: false,
+      previewPrice: false,
+      spaceDetails: true,
+    };
+  }
+
+  onSubmitPricing(): void {
+    this.steps = {
+      pricing: false,
+      previewPrice: true,
+      spaceDetails: false,
+    };
+    console.log('Form values ===>>', this.pricingForm.value);
   }
 }
